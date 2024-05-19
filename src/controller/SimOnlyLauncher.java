52fdc3145a3ae4bef4da7b8ec905df1331d018b8
opesnoir <goodsurroundsme@outlook.com>
2024-05-19 12:21:59 +0200

    feat: verbruik model

---
+++ src/controller/SimOnlyLauncher.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class SimOnlyLauncher {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+}
---
+++ src/model/Verbruik.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Verbruik {
+    //attributen
+    private final static double BASIS_KOSTEN = 9.95;
+    private final int VERBRUIK_ABBONEMENT = 3000;
+    private final double MEERPRIJS_MB = 0.025;
+    private String maand;
+    private int hoeveelheidMb;
+    private double kosten;
+
+    //constructors
+    public Verbruik(String maand, int hoeveelheidMb) {
+        this.maand = maand;
+        this.hoeveelheidMb = hoeveelheidMb;
+    }
+
+    public Verbruik(String maand) {
+        this(maand, 0);
+    }
+
+    public Verbruik() {
+        this("onbekend");
+    }
+
+    //getters, setters
+    public String getMaand() {
+        return maand;
+    }
+
+    public void setHoeveelheidMb(int hoeveelheidMb) {
+        this.hoeveelheidMb = hoeveelheidMb;
+    }
+
+    //methoden
+    public void berekenKosten() {
+        if (hoeveelheidMb <= VERBRUIK_ABBONEMENT) {
+            kosten = BASIS_KOSTEN;
+        } else {
+            int verbruikBuitenBundel = hoeveelheidMb - VERBRUIK_ABBONEMENT;
+            double kostenBuitenBundel = MEERPRIJS_MB * verbruikBuitenBundel;
+            kosten= BASIS_KOSTEN + kostenBuitenBundel;
+        }
+    }
+
+    public void toonVerbruik() {
+        System.out.printf("%8s %8d %.2f %s\n", maand, hoeveelheidMb, kosten, (kosten > BASIS_KOSTEN)? "buiten je bundel!" : " ");
+    }
+
+
+}
